b5975d3bc4add066113a069352d36ca845733ac4
Gabriel Drozd <drozdgabriel@pm.me>
2024-03-06 19:56:08 +0100

    Added Institution migration to Management module
    A new migration has been added to the Management module. This migration creates a new table, "Institution", and populates it with the necessary fields, such as "Address", "State", and "ShortName". Another table "InstitutionAdministratorIds" has also been created for maintaining relationships. The corresponding designer and snapshot files have been updated to reflect these changes.

---
+++ src/Modules/Management/Modules.Management.Infrastructure/Database/Migrations/20240306184934_Added_Institution.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Modules.Management.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Institution : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Management");
+
+            migrationBuilder.CreateTable(
+                name: "Institution",
+                schema: "Management",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    State = table.Column<int>(type: "integer", nullable: false),
+                    ShortName = table.Column<string>(type: "text", nullable: true),
+                    FullName = table.Column<string>(type: "text", nullable: true),
+                    Address_ZipCode = table.Column<string>(type: "text", nullable: true),
+                    Address_ZipCodeCity = table.Column<string>(type: "text", nullable: true),
+                    Address_City = table.Column<string>(type: "text", nullable: true),
+                    Address_HouseNumber = table.Column<string>(type: "text", nullable: true),
+                    Address_State = table.Column<string>(type: "text", nullable: true),
+                    Address_Street = table.Column<string>(type: "text", nullable: true),
+                    InstitutionId = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedOnUtc = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    ModifiedOnUtc = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    ModifiedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    DeletedOnUtc = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    DeletedBy = table.Column<Guid>(type: "uuid", nullable: true),
+                    Deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    xmin = table.Column<uint>(type: "xid", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Institution", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InstitutionAdministratorIds",
+                schema: "Management",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    InstitutionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AdministratorId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InstitutionAdministratorIds", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InstitutionAdministratorIds_Institution_InstitutionId",
+                        column: x => x.InstitutionId,
+                        principalSchema: "Management",
+                        principalTable: "Institution",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InstitutionAdministratorIds_InstitutionId",
+                schema: "Management",
+                table: "InstitutionAdministratorIds",
+                column: "InstitutionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InstitutionAdministratorIds",
+                schema: "Management");
+
+            migrationBuilder.DropTable(
+                name: "Institution",
+                schema: "Management");
+        }
+    }
+}
