3c15fec8ff6b5fb861fa181bc0ed7bad0b0cb8b3
Gabriel Drozd <drozdgabriel@pm.me>
2024-04-17 18:22:17 +0200

    Expanded Course class with additional properties
    The Course domain class has been significantly expanded. Additional properties and lists, such as LanguageLevel, MainTeacherId, StudentIds, and TeacherIds, have been added to better represent a course and its participants. This provides a more complete course representation within the Education module.

--- src/Modules/Education/Modules.Education.Domain/Courses/Course.cs
+++ src/Modules/Education/Modules.Education.Domain/Courses/Course.cs
@@ -1,21 +1,46 @@
 using Core.Domain.Primitives;
+using Core.Domain.Shared.EntityIds;
+using Core.Domain.Shared.Enumerations.Languages;
+using Core.Domain.Shared.ValueObjects;
 
 namespace Modules.Education.Domain.Courses;
 
 public sealed class Course : AggregateRoot<CourseId>
 {
-    public CourseCode Code { get; set; }
+    private readonly List<UserId> _studentIds = [];
+    private readonly List<UserId> _teacherIds = [];
+
+    public CourseCode Code { get; set; } = null!;
+    public LanguageLevel Level { get; private set; } = null!;
+
+    public string? Name { get; private set; }
+    public string? Description { get; private set; }
+
+    public Date? StartDate { get; private set; }
+    public Date? EndDate { get; private set; }
+
+    public UserId MainTeacherId { get; private set; } = default!;
+
+    public IReadOnlyList<UserId> TeacherIds => _teacherIds.AsReadOnly();
+    public IReadOnlyList<UserId> StudentIds => _studentIds.AsReadOnly();
 
     private Course()
     {
     }
 
-    private Course(CourseId courseId, CourseCode code)
+    private Course(CourseId courseId, CourseCode code, LanguageLevel level, UserId mainTeacherId, List<UserId> studentIds, List<UserId> teacherIds, InstitutionId institutionId)
         : base(courseId)
     {
         Code = code;
+        Level = level;
+        MainTeacherId = mainTeacherId;
+
+        _studentIds.AddRange(studentIds);
+        _teacherIds.AddRange(teacherIds);
+
+        SetInstitutionId(institutionId);
     }
 
-    public static Course Create(CourseId courseId, CourseCode code)
-        => new(courseId, code);
+    public static Course Create(CourseCode code, LanguageLevel level, UserId mainTeacherId, List<UserId> studentIds, List<UserId> teacherIds, InstitutionId institutionId)
+        => new(CourseId.New, code, level, mainTeacherId, studentIds, teacherIds, institutionId);
 }
