56d61545e53b9950d7c8da8e1876b3523953ab23
Gabriel Drozd <drozdgabriel@pm.me>
2024-05-01 18:21:48 +0200

    Refactor code for readability in Program.cs
    The commit includes minor changes to the 'Program.cs' file to improve code readability. The 'builder.Build().Run();' line has been divided into three separate lines for enhanced clarity and understanding of the code.

--- src/Enscool.Host/Program.cs
+++ src/Enscool.Host/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var bootstrapper = builder.AddProject<Projects.Enscool_Bootstrapper>("enscool-bootstrapper");
 
-builder.Build().Run();
+builder
+    .Build()
+    .Run();
