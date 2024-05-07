c8e4220ff79eadeebebddfbc5a38e4d1a3d8f7d1
Gabriel Drozd <drozdgabriel@pm.me>
2024-05-07 18:25:15 +0200

    Added courses list to Teacher entity
    A new private field of list of course IDs was included in the 'Teacher' class. A read-only public property has been provided to access this list. This change allows to maintain a list of courses associated with each teacher.

--- src/Modules/Education/Modules.Education.Domain/Teachers/Teacher.cs
+++ src/Modules/Education/Modules.Education.Domain/Teachers/Teacher.cs
@@ -1,10 +1,15 @@
 using Core.Domain.Primitives;
 using Core.Domain.Shared.EntityIds;
+using Modules.Education.Domain.Courses;
 
 namespace Modules.Education.Domain.Teachers;
 
 public sealed class Teacher : AggregateRoot<UserId>
 {
+    private readonly List<CourseId> _courses = [];
+
+    public IReadOnlyList<CourseId> Courses => _courses;
+
     private Teacher()
     {
     }
